44f7159c609d85c8ae582b9f86ffb4adc00b34df
lolf1819-ctrl <lolf1819@gmail.com>
2026-07-30 12:49:43 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert("🎉 Top Up Shob-এ স্বাগতম!");
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function () {
+        alert("✅ আপনার অর্ডার গ্রহণ করা হয়েছে। আমরা দ্রুত যোগাযোগ করব।");
+    });
+});
