70021ab6c66358833b8579ffeb60e957329a7638
lolf1819-ctrl <lolf1819@gmail.com>
2026-07-30 14:38:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("🎉 Top Up Shob-এ স্বাগতম!");
+const slides=document.querySelectorAll(".slide");
 
-    const form = document.querySelector("form");
+let current=0;
 
-    form.addEventListener("submit", function () {
-        alert("✅ আপনার অর্ডার গ্রহণ করা হয়েছে। আমরা দ্রুত যোগাযোগ করব।");
-    });
-});
+function showSlide(){
+
+slides.forEach(s=>s.classList.remove("active"));
+
+slides[current].classList.add("active");
+
+current++;
+
+if(current>=slides.length){
+
+current=0;
+
+}
+
+}
+
+setInterval(showSlide,3000);
